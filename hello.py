cc7c6aad64e57c5dbb7dd3c63bb6a65b41dd5a1e
Andjelaaaa <andjela.dimitrijevic@polymtl.ca>
2023-01-14 13:10:34 -0500

    Changement de l'exercice à réaliser

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-# Ajoutez une ligne qui affichera votre nom et prénom
+import math
+#TODO Affichez l'aire d'un cercle avec le rayon indiqué
+rayon = 4
+
+#TODO Ajoutez une ligne qui affichera votre nom et prénom à la fin
